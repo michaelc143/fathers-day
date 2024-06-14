b8a15d66ef2d07a37aa74445988ff12da5ebcf9e
Michael Corbishley <mcorbishley@wisc.edu>
2024-06-13 21:24:26 -0500

    init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className='flex  align-center justify-center h-screen'>
+      <h1 className='text-3xl font-bold my-4 text-stone-50'>Happy Fathers Day Dad!</h1>
+    </div>
+  );
+}
+
+export default App;
